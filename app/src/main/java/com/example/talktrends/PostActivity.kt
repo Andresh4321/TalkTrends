0715744591f70610963c9c376795a620595e7831
Andresh4321 <shahiandy123@gmail.com>
2024-12-31 12:36:30 +0545

    dashboard

--- app/src/main/java/com/example/talktrends/PostActivity.kt
+++ app/src/main/java/com/example/talktrends/PostActivity.kt
@@ -1,30 +1,20 @@
 package com.example.talktrends
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class PostActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-
-        // Setting edge-to-edge padding
+        setContentView(R.layout.activity_post)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        // Set button click listener to navigate to DashboardActivity
-        findViewById<Button>(R.id.button).setOnClickListener {
-            val intent = Intent(this, DashboardActivity::class.java)
-            startActivity(intent)
         }
     }
 }
