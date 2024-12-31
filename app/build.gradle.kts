cc3e6603f887202a507ab041ad6b7d8406042a6b
ravija357 <veerjai806@gmail.com>
2024-12-31 11:59:01 +0545

    Register

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.talktrends"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.talktrends"
--- app/src/main/java/com/example/talktrends/ExampleActivty.kt
+++ app/src/main/java/com/example/talktrends/ExampleActivty.kt
@@ -1,28 +1,20 @@
 package com.example.talktrends
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
-    lateinit var button: Button
+class ExampleActivty : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        button=findViewById(R.id.button)
+        setContentView(R.layout.activity_example_activty)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-        button.setOnClickListener{
-            val intent= Intent(this@MainActivity,RegistrationActivity::class.java)
-            startActivity(intent)
         }
     }
 }
