097cde7cc94272fc3255ceb96f81fe0e3cf45703
Andresh4321 <shahiandy123@gmail.com>
2025-01-18 13:55:20 +0545

    Add PostRepositoryImpl.kt

--- app/src/main/java/com/example/talktrends/Repositary/PostRepositoryImpl.kt
+++ app/src/main/java/com/example/talktrends/Repositary/PostRepositoryImpl.kt
@@ -1,4 +1,76 @@
 package com.example.talktrends.Repositary
 
-class PostRepositoryImpl {
+import com.example.talktrends.model.PostModel
+import com.example.talktrends.model.UserModel
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+
+class PostRepositoryImpl :PostRepository{
+
+    val database: FirebaseDatabase = FirebaseDatabase.getInstance()
+
+    val ref: DatabaseReference =database.reference.child("Posts")
+
+
+    override fun addPost(PostModel: PostModel, callback: (Boolean, String) -> Unit) {
+        var id=ref.push().key.toString()
+        PostModel.postId=id
+
+        ref.child(id).setValue(PostModel).addOnCompleteListener{
+            if(it.isSuccessful){
+                callback(true,"Post added successfully")
+            }else(
+                    callback(false,"${it.exception?.message}")
+                    )
+        }
+    }
+
+    override fun updatePost(
+        PostId: String,
+        data: MutableMap<String, Any>,
+        callback: (Boolean, String) -> Unit
+    ) {
+        ref.child(PostId).setValue(data).addOnCompleteListener{
+            if(it.isSuccessful){
+                callback(true,"Post added successfully")
+            }else(
+                    callback(false,"${it.exception?.message}")
+                    )
+        }
+    }
+
+    override fun deletePost(ProductId: String, callback: (Boolean, String) -> Unit) {
+
+    }
+
+    override fun getPostById(ProductId: String, callback: (PostModel?, Boolean, String) -> Unit) {
+
+    }
+
+
+    // Retrieve all posts
+    override fun getAllPosts(callback: (List<PostModel>?, Boolean, String) -> Unit){
+        ref.addValueEventListener(object:ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if(snapshot.exists()){
+                    var post= mutableListOf<PostModel>()
+                    for(eachData in snapshot.children){
+                        var model=eachData.getValue(PostModel::class.java)
+                       if(model!=null){
+                           post.add(model)
+                       }
+                    }
+                    callback(post,true,"Post fetched successfully")
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                callback(null, false, "Error fetching posts: ${error.message}")
+            }
+
+        })
+    }
 }
